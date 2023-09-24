46c3b0e776e7faad6a645225a20ad09ba010df29
cperki15 <cperki15@yahoo.com>
2023-09-23 21:17:47 -0700

    test 7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
